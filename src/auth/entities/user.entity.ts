3001486d82817952951d470bfce71bc0f7f5a568
AAGuedes <airam.a.g.85@gmail.com>
2023-06-06 19:16:43 +0100

    Crear modelo y esquema

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+
+    @Prop({ unique: true, required: true })
+    email: string;
+
+    @Prop({ default: true })
+    isActive: boolean;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ minlength: 6, required: true })
+    password: string;
+
+    @Prop({ type: [String], default: ['user'] })
+    roles: string[];
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
